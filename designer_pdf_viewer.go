edc5dfd94ae9a3d8c3938a2d8d187492d258b9a1
yessypp <purnamayessy@gmail.com>
2019-10-29 00:40:21 +0700

    Changes: - create designer_pdf_viewer.go

---
+++ designer_pdf_viewer.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+)
+
+// Complete the designerPdfViewer function below.
+func designerPdfViewer(h []int32, word string) int32 {
+    var max, count int32
+
+    for _, v := range word {
+        pos := v - 97
+        value := h[pos]
+
+        if value > max {
+            max = value
+        }
+
+        count++
+    }
+
+
+    return max * count
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 1024 * 1024)
+
+    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+    checkError(err)
+
+    defer stdout.Close()
+
+    writer := bufio.NewWriterSize(stdout, 1024 * 1024)
+
+    hTemp := strings.Split(readLine(reader), " ")
+
+    var h []int32
+
+    for i := 0; i < 26; i++ {
+        hItemTemp, err := strconv.ParseInt(hTemp[i], 10, 64)
+        checkError(err)
+        hItem := int32(hItemTemp)
+        h = append(h, hItem)
+    }
+
+    word := readLine(reader)
+
+    result := designerPdfViewer(h, word)
+
+    fmt.Fprintf(writer, "%d\n", result)
+
+    writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
